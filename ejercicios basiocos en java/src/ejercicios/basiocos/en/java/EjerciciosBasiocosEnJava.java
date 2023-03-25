9c5032e553bffa9c2e4b144387297a58aa4862a8
gimenavargas <1471366@senati.pe>
2023-03-25 08:23:38 -0500

    ejercicio 1 de java

---
+++ ejercicios basiocos en java/src/ejercicios/basiocos/en/java/EjerciciosBasiocosEnJava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios.basiocos.en.java;
+
+/**
+ *
+ * @author Alumno
+ */
+public class EjerciciosBasiocosEnJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //
+        System.out.println("Morning,Morning,Morning      ");
+        System.out.println("      Hello,Have a good day  ");
+        System.out.println("             oh! Thank you   ");
+        System.out.println("                             ");
+        System.out.println("             you are great   ");
+        System.out.println("      This is my pleasure    ");
+        System.out.println("No,... You are Welcome       ");
+    }
+   
+}
